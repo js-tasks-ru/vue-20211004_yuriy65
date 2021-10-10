1fad3e2546566e3d7c854314c67c635ed6536d6f
Yury Nesterenko <7710358@gmail.com>
2021-10-10 23:03:08 +0300

    04-MeetupTitle: Solve meetups change

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,24 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const component = defineComponent({
+  data() {
+    return {
+      selectedNum: 1,
+      title: '',
+    };
+  },
+
+  watch: {
+    selectedNum: {
+      immediate: true,
+      handler() {
+        fetchMeetupById(this.selectedNum).then((meetup) => {
+          this.title = meetup.title;
+        });
+      },
+    },
+  },
+});
+
+createApp(component).mount('#app');
