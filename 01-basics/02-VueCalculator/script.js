9182d79bb8e0263c077192783d25c02071e725a0
Yury Nesterenko <7710358@gmail.com>
2021-10-10 21:13:21 +0300

    02-VueCalculator: Solve operations

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,48 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const operations = [
+  {
+    value: 'sum',
+    symbol: '➕',
+    operation: (a, b) => a + b,
+  },
+  {
+    value: 'subtract',
+    symbol: '➖',
+    operation: (a, b) => a - b,
+  },
+  {
+    value: 'multiply',
+    symbol: '✖',
+    operation: (a, b) => a * b,
+  },
+  {
+    value: 'divide',
+    symbol: '➗',
+    operation: (a, b) => (b !== 0 ? a / b : 'Division by zero!'),
+  },
+];
+
+const rootComponent = defineComponent({
+  data() {
+    return {
+      selectedOperation: operations[0],
+      leftOperand: 0,
+      rightOperand: 0,
+    };
+  },
+
+  computed: {
+    operations() {
+      return operations;
+    },
+
+    result() {
+      const { selectedOperation, leftOperand, rightOperand } = this;
+
+      return selectedOperation.operation(+leftOperand, +rightOperand);
+    },
+  },
+});
+
+createApp(rootComponent).mount(document.getElementById('app'));
