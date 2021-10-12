d60edc91f9480fb1b5fb3e72e505dd3ddd3d6ff8
Yury Nesterenko <7710358@gmail.com>
2021-10-12 23:52:22 +0300

    01-CounterButtonComponent: Solve

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,23 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+  },
+
+  emits: ['update'],
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    increaseCounter() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="increaseCounter">{{ count }}</button>`,
 });
